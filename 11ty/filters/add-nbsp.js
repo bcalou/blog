c789c2ce9c18320be9039d96809038f5a15328e6
bcalou <bastien.calou@gmail.com>
2022-12-05 19:09:58 +0100

    [addNbsp] add ; to charlist

--- 11ty/filters/add-nbsp.js
+++ 11ty/filters/add-nbsp.js
@@ -8,6 +8,7 @@ function addNbsp(text) {
     .replace(new RegExp(/\s\!/, 'g'), '&nbsp;!')
     .replace(new RegExp(/\s\?/, 'g'), '&nbsp;?')
     .replace(new RegExp(/\s\:/, 'g'), '&nbsp;:')
+    .replace(new RegExp(/\s\;/, 'g'), '&nbsp;;')
     .replace(new RegExp(/\«\s/, 'g'), '«&nbsp;')
     .replace(new RegExp(/\s\»/, 'g'), '&nbsp;»')
 }
