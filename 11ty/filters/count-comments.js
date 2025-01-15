336b267184e93afdac67c82f97a99c338a20ee6a
bcalou <bastien.calou@gmail.com>
2025-01-15 18:37:22 +0100

    retirer contenu

--- 11ty/filters/count-comments.js
+++ 11ty/filters/count-comments.js
@@ -1,5 +1,4 @@
 function countComments(comments, page) {
-  console.log(comments);
   return Array.isArray(comments)
     ? comments.filter((comment) => comment.data.referrer.includes(page.url))
         .length
--- 11ty/filters/get-answers.js
+++ 11ty/filters/get-answers.js
@@ -1,5 +1,4 @@
 function getAnswers(comments, to) {
-  console.log("getAnswers", comments);
   return Array.isArray(comments)
     ? comments.filter((comment) => comment.data.answering === to).reverse()
     : [];
