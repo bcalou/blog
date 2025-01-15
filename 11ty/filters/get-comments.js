de0822e1c45fe4b1521f812d51df69936efcbd64
bcalou <bastien.calou@gmail.com>
2025-01-15 17:52:49 +0100

    fix comments logic

--- 11ty/filters/get-comments.js
+++ 11ty/filters/get-comments.js
@@ -1,24 +1,12 @@
 function getComments(comments, page) {
-  return comments
-    .filter(
-      (comment) =>
-        comment.data.referrer.includes(page.url) && !comment.data.answering
-    )
-    .reverse();
-
-  // pageComments.forEach((comment) => {
-  //   if (comment.data.answering) {
-  //     const to = pageComments.find(
-  //       (comment) => comment.id === comment.data.answering
-  //     );
-  //     if (to) {
-  //       if (!to.answers) {
-  //         to.answers = [];
-  //       }
-  //       to.answers.push(comment);
-  //     }
-  //   }
-  // });
+  return Array.isArray(comments)
+    ? comments
+        .filter(
+          (comment) =>
+            comment.data.referrer.includes(page.url) && !comment.data.answering
+        )
+        .reverse()
+    : [];
 }
 
 module.exports = getComments;
